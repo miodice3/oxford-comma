eccccb403ab8175707bed3381ea87fc09f0a09c5
Michael Iodice <miodice3@gmail.com>
2020-09-30 12:44:38 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    num_indexs = array.size
+    if num_indexs >= 3
+        last_element = array.pop
+        comma_array = array
+        return comma_array.join(", ")<<", and #{last_element}"
+    elsif num_indexs == 2
+        last_element = array.pop
+        comma_array = array
+        return comma_array.join<<" and #{last_element}"
+    elsif num_indexs <=1
+        return array.pop
+    end
 end
